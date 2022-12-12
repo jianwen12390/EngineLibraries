a834d5e17bc03f89340dbc4d4b5a4e8fe51e4317
jianwen12390 <48422533+jianwen12390@users.noreply.github.com>
2022-12-12 16:34:00 +0800

    Assimp

---
+++ Assimp/BINARIES/Win32/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jw/Desktop/Github_Desktop_Folder/Huat-Studio/dependencies/assimp/contrib/zlib
+# Build directory: C:/Users/jw/Desktop/Github_Desktop_Folder/Huat-Studio/dependencies/assimp/BINARIES/Win32/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
